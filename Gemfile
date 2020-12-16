318338ad664e3890df63c4c5b0bd669be842a15a
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-12-16 16:13:01 +0900

    implement basic login

--- Gemfile
+++ Gemfile
@@ -70,7 +70,7 @@ group :production do
   gem 'pg', '0.20.0'
 end
 
-
+gem 'jquery-rails'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
